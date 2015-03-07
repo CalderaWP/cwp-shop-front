5896c9407f08ed09d712b42e387231efc6053c68
David Cramer <david@digilab.co.za>
2015-03-07 08:23:54 +0200

    load correct style

--- functions.php
+++ functions.php
@@ -45,9 +45,9 @@ define( 'CWP_SF_CHILD_VERSION', '0.1.0' );
 	wp_deregister_style( 'style' );
 	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
 
-	wp_enqueue_script( 'cwp_sf_child', get_template_directory_uri() . "/assets/js/cwp_theme_front_child_theme{$postfix}.js", array(), CWP_SF_CHILD_VERSION, true );
+	wp_enqueue_script( 'cwp_sf_child', get_stylesheet_directory_uri() . "/assets/js/cwp_theme_front_child_theme{$postfix}.js", array(), CWP_SF_CHILD_VERSION, true );
 		
-	wp_enqueue_style( 'cwp_sf_child', get_template_directory_uri() . "/assets/css/cwp_theme_front_child_theme{$postfix}.css", array(), CWP_SF_CHILD_VERSION );
+	wp_enqueue_style( 'cwp_sf_child', get_stylesheet_directory_uri() . "/assets/css/cwp_theme_front_child_theme{$postfix}.css", array(), CWP_SF_CHILD_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'cwp_sf_child_scripts_styles', 21 );
  
@@ -55,7 +55,7 @@ define( 'CWP_SF_CHILD_VERSION', '0.1.0' );
   * Add humans.txt to the <head> element.
   */
  function cwp_sf_child_header_meta() {
-	$humans = '<link type="text/plain" rel="author" href="' . get_template_directory_uri() . '/humans.txt" />';
+	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
 	
 	echo apply_filters( 'cwp_sf_child_humans', $humans );
  }
