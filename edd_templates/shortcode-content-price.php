72436eafaec7493558f075d3cd7a8879c3fcdfa6
David Cramer <david@digilab.co.za>
2015-03-07 10:45:22 +0200

    left download sb

---
+++ edd_templates/shortcode-content-price.php
@@ -0,0 +1,60 @@
+<?php 
+	
+/**		
+ * Coming soon downloads
+*/
+
+do_action( 'shopfront_the_title' ); // The shopfront_render_the_title() function is loaded on this hook
+
+echo '<p class="download-exceprt">' . get_the_excerpt() . '</p>';
+
+$coming_soon = isset( $post->ID ) ? get_post_meta( $post->ID, 'edd_coming_soon', true ) : '';
+$coming_soon_text = isset( $post->ID ) ? get_post_meta( $post->ID, 'edd_coming_soon_text', true ) : '';
+
+if ( $coming_soon ) : ?>
+	
+	<?php if ( $coming_soon_text ) : ?>
+
+	<div itemprop="price" class="edd_price">
+		<?php echo $coming_soon_text; ?>
+	</div>
+
+	<?php else : ?>
+
+		<div itemprop="price" class="edd_price">
+			<?php _e( 'Coming soon', 'shop-front' ); ?>
+		</div>
+
+	<?php endif; ?>
+
+<?php
+/**		
+ * Free downloads
+*/
+elseif ( '0' == edd_get_download_price( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) : ?>
+	<div itemprop="price" class="edd_price">
+		<?php _e( 'Free', 'shop-front' ); ?>
+	</div>
+
+
+<?php 
+/**		
+ * Variable priced downloads
+*/
+elseif ( edd_has_variable_prices( get_the_ID() ) ) : ?>
+
+	<div itemprop="price" class="edd_price">
+		<?php _e( 'From', 'shop-front' ); ?> <?php edd_price( get_the_ID() ); ?>
+	</div>
+
+<?php 
+/**		
+ * Else show the normal price
+*/
+else : ?>
+
+	<div itemprop="price" class="edd_price">
+		<?php edd_price( get_the_ID() ); ?>
+	</div>
+
+<?php endif; ?>
---
+++ single-download.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Single download pages
+ */
+	
+get_header(); ?>
+
+<?php do_action( 'shopfront_single_download_start' ); ?>
+<?php
+/**		
+ * Secondary Sidebar
+*/
+if ( !is_page_template( 'page-templates/fullwidth.php' ) )
+	get_sidebar( 'download' );
+?>
+<section id="primary" class="primary-right">
+
+	<?php do_action( 'shopfront_single_download_primary_start' ); ?>
+
+	<?php do_action( 'shopfront_single_download_image' ); // The single download image is loaded on this hook ?>
+
+	<?php do_action( 'shopfront_single_download_content_before' ); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'download' ); ?>
+
+		<?php do_action( 'shopfront_single_download_content_after' ); ?>
+
+		<?php comments_template( '', true );  ?>
+
+	<?php endwhile; // end of the loop. ?>
+
+	<?php do_action( 'shopfront_single_download_primary_end' ); ?>
+
+</section>
+
+<?php do_action( 'shopfront_single_download_end' ); ?>
+
+
+
+<?php get_footer(); ?>
