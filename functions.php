74b70dc685c777f5c94ba1d32cc75dd124e9b681
Josh Pollock <jpollock412@gmail.com>
2015-04-06 16:05:49 -0400

    Filter to lower prices by $1

--- functions.php
+++ functions.php
@@ -197,6 +197,19 @@ add_action( 'init', function() {
 	}, 50 );
 });
 
+add_filter( 'edd_get_variable_prices', function( $prices, $download_id ) {
+
+	foreach( $prices as $i => $price ) {
+		$this_price = absint( $price[ 'amount' ] );
+		if ( 0 < $this_price ) {
+			$this_price = (int) $this_price - 1;
+			$prices[ $i ][ 'amount' ] = edd_sanitize_amount( $this_price );
+		}
+
+	}
+
+	return $prices;
+}, 99, 2 );
 
 /**
  * Move soical discounts up.
