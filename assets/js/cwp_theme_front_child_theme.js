710b0b932d9f2d550c4ec8598a98b4c954608e02
Josh Pollock <jpollock412@gmail.com>
2015-05-10 20:53:12 -0400

    Testimonial styles

--- assets/js/cwp_theme_front_child_theme.js
+++ assets/js/cwp_theme_front_child_theme.js
@@ -1,3 +1,3 @@
-/*! CWP Theme Front Child Theme - v0.1.0 - 2015-04-15
+/*! CWP Theme Front Child Theme - v0.1.0 - 2015-05-10
  * https://github.com/CalderaWP/cwp-shop-front
  * Copyright (c) 2015; * Licensed GPLv2+ */
