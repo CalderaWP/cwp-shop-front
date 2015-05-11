9e5fa4f230987fd063b6b9997e985ec4a729f7ec
Josh Pollock <jpollock412@gmail.com>
2015-05-10 20:31:54 -0400

    add drip code

--- functions.php
+++ functions.php
@@ -318,3 +318,23 @@ add_action( 'after_setup_theme', function() {
 
 }, 50 );
 
+/**
+ * Drip code
+ */
+ add_action( 'wp_footer', function() {
+	?>
+<!-- Drip -->
+<script type="text/javascript">
+	var _dcq = _dcq || [];
+	var _dcs = _dcs || {};
+	_dcs.account = '4342573';
+
+	(function() {
+		var dc = document.createElement('script');
+		dc.type = 'text/javascript'; dc.async = true;
+		dc.src = '//tag.getdrip.com/4342573.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(dc, s);
+	})();
+</script><?php
+ });
