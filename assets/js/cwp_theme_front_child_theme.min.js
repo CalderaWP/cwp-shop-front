ba10e9cf3b2e377546eddd46f3988b79e29a30c9
Josh Pollock <jpollock412@gmail.com>
2015-07-22 02:20:38 -0400

    fix lost front page CSS

--- assets/js/cwp_theme_front_child_theme.min.js
+++ assets/js/cwp_theme_front_child_theme.min.js
@@ -1,3 +1,3 @@
-/*! CWP Theme Front Child Theme - v0.1.0 - 2015-05-14
+/*! CWP Theme Front Child Theme - v0.1.0 - 2015-07-22
  * https://github.com/CalderaWP/cwp-shop-front
  * Copyright (c) 2015; * Licensed GPLv2+ */
