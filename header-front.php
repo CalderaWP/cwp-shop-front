b7bb1a58a6a8b425f1e96a6d076dafe763da013b
Josh Pollock <jpollock412@gmail.com>
2015-07-22 00:32:55 -0400

    make custom header for front

--- header-front.php
+++ header-front.php
@@ -26,14 +26,55 @@
 
 <div id="site" class="hfeed">
 
+<?php do_action( 'shopfront_site_start' ); ?>
 
+	<?php do_action( 'shopfront_header_before' ); ?>
+
+	<header id="masthead" role="banner" class="front-header">
+
+		<?php do_action( 'shopfront_header_start' ); ?>
+
+		<div class="wrapper">
+
+			<?php do_action( 'shopfront_header_wrapper_start' ); ?>
+
+			<?php
+				$tagline = get_bloginfo( 'description' );
+
+				$output = '
+				<hgroup>
+					<h1 id="site-title" class="site-title-front">
+						<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) .'" rel="home">'. get_bloginfo( 'name' ) . '</a>
+					</h1>
+					<h2 id="site-description">' . get_bloginfo( 'description' ) .'</h2>
+				</hgroup>
+				';
+			
+				echo  $output;
+				?>
+
+				<?php do_action( 'shopfront_header_site_title_end' ); ?>
+
+			<?php do_action( 'shopfront_header_wrapper_end' ); ?>
+
+		</div>
+
+
+	</header>
+
+	<?php do_action( 'shopfront_header_after' ); ?>
+
+
+<?php do_action( 'shopfront_container_before' ); ?>
 
 	<div id="container" class="front-container">
-
+	<?php do_action( 'shopfront_container_start' ); ?>
 
 		<div class="wrapper" class="front-wrapper">
+		<?php do_action( 'shopfront_container_wrapper_start' ); ?>
 
+		<?php do_action( 'shopfront_content_before' ); ?>
 
 			<div id="content" class="front-content">
 
-
+			<?php do_action( 'shopfront_content_start' ); ?>
