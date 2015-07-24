e56e6c56e482c3c6b0c7510e57a7ee9d725e741e
Josh Pollock <jpollock412@gmail.com>
2015-07-24 01:04:41 -0400

    reduce front-page section 1 spacing

--- assets/js/cwp_theme_front_child_theme.js
+++ assets/js/cwp_theme_front_child_theme.js
@@ -1,3 +1,3 @@
-/*! CWP Theme Front Child Theme - v0.1.0 - 2015-07-22
+/*! CWP Theme Front Child Theme - v0.1.0 - 2015-07-24
  * https://github.com/CalderaWP/cwp-shop-front
  * Copyright (c) 2015; * Licensed GPLv2+ */
