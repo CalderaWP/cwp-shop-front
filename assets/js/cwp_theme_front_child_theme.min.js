e256f890d75e198bcacbe1f98f5a337d32ca0f3a
Josh Pollock <jpollock412@gmail.com>
2015-08-13 19:18:31 -0400

    author info in single post view

--- assets/js/cwp_theme_front_child_theme.min.js
+++ assets/js/cwp_theme_front_child_theme.min.js
@@ -1,3 +1,3 @@
-/*! CWP Theme Front Child Theme - v0.1.0 - 2015-07-24
+/*! CWP Theme Front Child Theme - v0.1.0 - 2015-08-13
  * https://github.com/CalderaWP/cwp-shop-front
  * Copyright (c) 2015; * Licensed GPLv2+ */
